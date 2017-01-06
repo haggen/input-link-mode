033657ff02552b3c9444e8390d975caab76738db
Arthur Corenzan <haggen@users.noreply.github.com>
2017-01-06 15:44:58 -0200

    Version one point o

---
+++ input-link-mode.js
@@ -0,0 +1,69 @@
+// input-link-mode.js v1.0.0
+// more on https://github.com/haggen/input-link-mode
+;(function(root) {
+  function setLinkMode(input) {
+    input.classList.add('link-mode');
+    input.isLinkMode = true;
+  }
+
+  function unsetLinkMode(input) {
+    input.classList.remove('link-mode');
+    input.isLinkMode = false;
+  }
+
+  function setupLinkMode(input) {
+    var onInputMouseEnter = function onInputMouseEnter(e) {
+      input.hasMouseOver = true;
+      if (e.ctrlKey || e.metaKey) {
+        setLinkMode(input);
+      }
+    };
+    
+    var onInputMouseOut = function onInputMouseOut(e) {
+      input.hasMouseOver = false;
+      if (input.isLinkMode) {
+        unsetLinkMode(input);
+      }
+    };
+    
+    var onInputClick = function onInputClick(e) {
+      if (input.isLinkMode) {
+        var url = input.value;
+        if (url.indexOf('http') !== 0) url = 'http://' + url;
+        window.open(url, '_blank');
+      }
+    };
+    
+    var onDocumentKeyDown = function onDocumentKeyDown(e) {
+      if (input.hasMouseOver && (e.ctrlKey || e.metaKey)) {
+        setLinkMode(input);
+      }
+    };
+    
+    var onDocumentKeyUp = function onDocumentKeyUp(e) {
+      if (input.isLinkMode) {
+        unsetLinkMode(input);
+      }
+    };
+
+    input.addEventListener('mouseenter', onInputMouseEnter);
+    input.addEventListener('mouseout', onInputMouseOut);
+    input.addEventListener('click', onInputClick);
+    document.addEventListener('keydown', onDocumentKeyDown);
+    document.addEventListener('keyup', onDocumentKeyUp);
+
+    input.removeLinkMode = function removeLinkMode() {
+      input.addEventListener('mouseenter', onInputMouseEnter);
+      input.addEventListener('mouseout', onInputMouseOut);
+      input.addEventListener('click', onInputClick);
+      document.addEventListener('keydown', onDocumentKeyDown);
+      document.addEventListener('keyup', onDocumentKeyUp);
+    };
+  }
+
+  root.removeLinkMode = function removeLinkMode(input) {
+    input.removeLinkMode();
+  };
+
+  root.setupLinkMode = setupLinkMode;
+})(this);
